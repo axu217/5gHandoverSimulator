da44926ebf27bb1ae2b8e7637c83c233456d030d
Zhecheng Xu <albertxu217@gmail.com>
2019-12-12 18:52:12 -0800

    Added more skeleton code

--- simulation.py
+++ simulation.py
@@ -6,17 +6,23 @@ class Point:
 class Node:
     def __init__(self, location):
         self.location = location
+        self.signalStrength = 0
 
 class BaseStation(Node):
     def __init__(self, location):
         self.ueList = []
         super(BaseStation, self).__init__(location)
         
+    #TODO: What shape do we set our boundaries? 4G is hexagon but that's hard to code. Squares?
     def isUeInRange(self, userEquipment):
-        #TODO: What shape do we set our boundaries? 4G is hexagon but that's hard to code. Circle?
-        return true
+        return True
     
-    def addInRangeUE(self, userEquipment)
+    #TODO: Account for distance as well as BaseStation number of equipments
+    def getBandwidth(self, userEquipment):
+        return 1
+
+    def addInRangeUE(self, userEquipment):
+        self.ueList.append(userEquipment)
         
 
 class UserEquipment(Node):
@@ -33,6 +39,8 @@ baseStationLocations = [
 
 endDevices = []
 baseStations = []
+
+#Initializing nodes
 for i in range(0, numberOfEndDevices):
     endDevice = UserEquipment(Point(i, 0))
     endDevices.append(endDevice)
@@ -41,6 +49,7 @@ for i in range(0, numberOfBaseStations):
     baseStation = BaseStation(baseStationLocations[1])
     baseStations.append(baseStation)
 
+#Assigning nodes to base stations
 for i in range(0, numberOfEndDevices):
     currentEndDevice = endDevices[1]
     for j in range(0, numberOfBaseStations):
@@ -48,4 +57,15 @@ for i in range(0, numberOfEndDevices):
         if(currentBaseStation.isUeInRange(currentEndDevice)):
             currentBaseStation.addInRangeUE(currentEndDevice)
             break
+
+#Calculating signal strengths base off of naive assignment
+unoptimizedBandwidths = []
+for i in range(0, numberOfBaseStations):
+    currentBaseStation = baseStations[i]
+    currentEndDevices = currentBaseStation.ueList
+    for j in range(0, len(currentEndDevices)):
+        currentDevice = currentEndDevices[j]
+        bandwidth = currentBaseStation.getBandwidth(currentDevice)
+        unoptimizedBandwidths.append(bandwidth)
+
 
